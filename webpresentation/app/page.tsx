2fa58ff777bf09d0fbff5c44f3c54106b448541c
Marius Jahn <Marius.jahn@5minds.de>
2022-11-12 17:58:05 +0100

    more socials

--- webpresentation/app/page.tsx
+++ webpresentation/app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
             <Image className='profile-picture' alt='Picture of me' src={profilePicture}></Image>
             <div className='socialBar'>
                 <a href="https://www.linkedin.com/in/marius-jahn-743992225/" className="fa fa-linkedin "/>
+                <a href="https://www.xing.com/profile/Marius_Jahn5/cv" className="fa fa-xing"></a>
+                <a href="https://github.com/MariusJahn" className="fa fa-github"></a>
             </div>
             <div className="content">
                 <div className="column">
