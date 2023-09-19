dc1f4d3e754b0124f854a19ec8df8d7b2ce05984
Marius Jahn <Mariusjahn@web.de>
2023-09-19 20:59:51 +0200

    new setup

--- webpresentation/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {appDir: true},
-  distDir: 'build',
-}
-
-module.exports = nextConfig
